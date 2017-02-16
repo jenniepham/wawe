4ffe1d8ec5490820726d1c28199e64289fefa272
Jennie <jenniepham0221@gmail.com>
2017-02-16 22:47:52 +0000

    css

--- client/js/index.js
+++ client/js/index.js
@@ -25,15 +25,15 @@ $('#generate').on('click', function(){
    var numPPL = numppl.value;
    numPPL = parseInt(numPPL);
    if(numPPL>10){
-       $('#instruction').html("There are too many of you. Regardless of how we do this, at least one person will be unhappy. "
-       + "So, here are even less choices. Let the most picky people remove all but one item.");
+       $('#instruction').html("<p class='text'>There are too many of you. Regardless of how we do this, at least one person will be unhappy. "
+       + "So, here are even less choices. Let the most picky people remove all but one item.</p>");
        numPPL = 4;
    }
    else if(numPPL==1){
-        $('#instruction').html("You have two choices. Discard one.");
+        $('#instruction').html("<p class='text'>You have two choices. Discard one.</p>");
    }
    else{
-   $('#instruction').html("Everyone must take turn and remove one item.");
+   $('#instruction').html("<p class='text'>Everyone must take turn and remove one item.</p>");
    }
    var i = 0;
       while (i<=numPPL){
@@ -57,14 +57,22 @@ $('#generate').on('click', function(){
    
    if(result.length == 1){
       $('.result').html("<button class='choice'>" + result[0] + "</button> <button class='final'><</button>"  + "<br>");
-      $('.result').append("<br><br>Enter your location to find " + result[0] + " restaurants near you.<br><br>");
+      $('.result').append("<br><br><p class='text'>Enter your location to find " + result[0] + " restaurants near you.</p><br>");
       $('.result').append("<input id='search' type='text'/> ");
       $('.result').append(" <button id='yelp'>Yelp!</button>");
    }
    
    $('#yelp').on('click',function(){
        var location = $('#search').val();
-       window.open("https://www.yelp.com/search?find_desc=Restaurants&find_loc=" + location + ",+TX&start=0&cflt=" + result[0]);
+       var food = result[0];
+       if (food == 'Fish & Chips'){
+           food = 'fishnchips';
+       }
+       else if (food == 'Chicken Wings'){
+           food = 'chicken_wings';
+       }
+       window.open("https://www.yelp.com/search?find_desc=Restaurants&find_loc=" + location + ",+TX&start=0&cflt=" + food);
+       
    });
    
 });
